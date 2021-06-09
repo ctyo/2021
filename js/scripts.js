970e4e3332704b6ae4fc089a688a46da4fdd5611
ctyo <ctyo@users.noreply.github.com>
2021-06-09 16:37:14 +0000

    deploy: e56eabcdba2e678f449c5fb76805b2e769319199

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
 /*!
-* Start Bootstrap - Coming Soon v6.0.2 (https://startbootstrap.com/theme/coming-soon)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
+* Start Bootstrap - foss4g_tokai_2021 v6.0.2 (undefined)
+* Copyright 2013-2021 undefined
+* Licensed under MIT (https://github.com/StartBootstrap/foss4g_tokai_2021/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+fetch('./assets/timetable.json')
+.then((response) => {
+    if (!response.ok) {
+        throw new Error();
+    }
+    return response.json()
+})
+.then((json) => {
+    console.dir(json);
+    const table = document.querySelector('#timetable');
+    json.forEach(row => {
+        const newRow = table.insertRow();
+
+        let newCell = newRow.insertCell();
+        newCell.appendChild(document.createTextNode(row.time));
+        newCell = newRow.insertCell();
+
+        let text = row.title1 + (row.author1 == '' ? '' : ' \n(' + row.author1 + ')');
+        newCell.appendChild(document.createTextNode(text));
+        newCell = newRow.insertCell();
+        let text2 = row.title2 + (row.author2 == '' ? '' : ' \n(' + row.author2 + ')');
+        newCell.appendChild(document.createTextNode(text2));
+    });
+    console.dir(json)
+})
+
