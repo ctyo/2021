e56eabcdba2e678f449c5fb76805b2e769319199
ctyo <ctyo@live.jp>
2021-06-10 01:36:31 +0900

    タイムテーブルをGoogleSheet管理に変更

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,26 @@
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+fetch('./assets/timetable.json')
+.then((response) => {
+    if (!response.ok) {
+        throw new Error();
+    }
+    return response.json()
+})
+.then((json) => {
+    console.dir(json);
+    const table = document.querySelector('#timetable');
+    json.forEach(row => {
+        const newRow = table.insertRow();
+
+        let newCell = newRow.insertCell();
+        newCell.appendChild(document.createTextNode(row.time));
+        newCell = newRow.insertCell();
+
+        let text = row.title1 + (row.author1 == '' ? '' : ' \n(' + row.author1 + ')');
+        newCell.appendChild(document.createTextNode(text));
+        newCell = newRow.insertCell();
+        let text2 = row.title2 + (row.author2 == '' ? '' : ' \n(' + row.author2 + ')');
+        newCell.appendChild(document.createTextNode(text2));
+    });
+    console.dir(json)
+})
+
